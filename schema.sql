b3c5fe6bda0e740d38847f94eaad1a03bff304ef
meghan <m.mwizerwa@alustudent.com>
2026-01-20 20:40:09 +0200

    new changes

--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ USE motodb;
 CREATE TABLE riders (
 	rider_id INT PRIMARY KEY,
 	name VARCHAR(100),
-	motorcycle_plate VARCHAR(100),
+	motorcycle_plate VARCHAR(100)
 );
 
 CREATE TABLE passengers (
 	passenger_id INT PRIMARY KEY,
 	name VARCHAR(100),
-	phone VARCHAR(100),
+	phone VARCHAR(100)
 );
 
 CREATE TABLE trips (
@@ -19,8 +19,6 @@ CREATE TABLE trips (
 	passenger_id INT,
 	fare DECIMAL,
 	trip_date DATETIME,
-	FOREIGN KEY (rider_id),
-	REFERENCES riders(rider_id),
-	FOREIGN KEY (passenger_id),
-	REFERENCES passengers(passenger_id),
+	FOREIGN KEY (rider_id)	REFERENCES riders(rider_id),
+	FOREIGN KEY (passenger_id) REFERENCES passengers(passenger_id)
 );
